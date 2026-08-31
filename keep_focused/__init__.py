572691140b9a2a41bd0c1ba7d44d6c7931312820
xweinp <xweinp@gmail.com>
2026-09-01 00:14:55 +0200

    chore: bump to 0.2.5

--- keep_focused/__init__.py
+++ keep_focused/__init__.py
@@ -1,6 +1,6 @@
 """keep-focused – block distracting websites system-wide."""
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 # Suggested sites shown during `keep-focused setup`
 SUGGESTED_SITES = [
